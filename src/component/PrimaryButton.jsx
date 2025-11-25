eb1e115b665e848e353ac0ebe173276d2c3ec70b
kamboh164 <af2747767@gmail.com>
2025-11-25 14:10:38 +0500

    changes

--- src/component/PrimaryButton.jsx
+++ src/component/PrimaryButton.jsx
@@ -14,7 +14,7 @@ export default function PrimaryButton({
       disabled={disabled}
       className={`${
         fullWidth ? 'w-full' : 'px-6'
-      } py-3 primary-color text-white font-medium rounded-lg  focus:outline-none focus:ring-2 focus:ring-offset-2 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed`}
+      } py-3 primary-color text-white font-medium rounded-lg  focus:outline-none focus:ring-2 focus:ring-offset-2 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer`}
     >
       {children}
     </button>
