90afd8099b7e3ff29abb4716c857a1a073eb6a2e
Usamaa <usamaa.mangocoders@gmail.com>
2025-11-26 12:54:11 +0500

    small change in login

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function login() {
     <>
       <Header />
       <div className="min-h-[calc(100vh-80px)] flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 px-4">
-        <AuthGuard>
+        <AuthGuard requireAuth={false}>
         <LoginForm />
         </AuthGuard>
       </div>
